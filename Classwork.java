b8b71b47d28b91cf0ec78f94e60eb90865516849
jbmdevelopment <jbarreramartinez100@student.hbuhsd.edu>
2024-09-30 11:05:24 -0700

    sasasasasdfasfa

--- Classwork.java
+++ Classwork.java
@@ -1,5 +1,11 @@
 public class Classwork
 {
+  public static String DelEnd(String msg, int n) {
+   String msg1 = new String(msg);
+   int len = msg1.length();
+   String ez = msg1.substring(0, len - n);
+   return ez;
+  }
   public static String FirstLastN(String msg, int n) {
       String msg1 = new String(msg);
       String first = msg1.substring(0, n);
@@ -14,6 +20,8 @@ public class Classwork
     String firstLast2 = FirstLastN("apple", 2);
     System.out.println(firstLast);
     System.out.println(firstLast2);
+    String secondQuestion = DelEnd("library", 5);
+    System.out.println(secondQuestion);
     // test your code here
   }
 
