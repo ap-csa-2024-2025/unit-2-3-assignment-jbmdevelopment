944b233f569d0f7fe287355722c575855276a5e2
jbmdevelopment <jbarreramartinez100@student.hbuhsd.edu>
2024-09-30 10:59:33 -0700

    aaaaaaa

--- Classwork.java
+++ Classwork.java
@@ -1,7 +1,19 @@
 public class Classwork
 {
+  public static String FirstLastN(String msg, int n) {
+      String msg1 = new String(msg);
+      String first = msg1.substring(0, n);
+      int len = msg1.length();
+      String last = msg1.substring(len - n);
+      String concatted = first + last;
+      return concatted;
+  }
   public static void main(String[] args)
   {
+    String firstLast = FirstLastN("dictionary", 1);
+    String firstLast2 = FirstLastN("apple", 2);
+    System.out.println(firstLast);
+    System.out.println(firstLast2);
     // test your code here
   }
 
